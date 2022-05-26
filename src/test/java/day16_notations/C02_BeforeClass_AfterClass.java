638d52fe491d223f9fe8a15b8918b23fb05f6c3f
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-05-26 22:26:48 +0300

    26.05.2022

---
+++ src/test/java/day16_notations/C02_BeforeClass_AfterClass.java
@@ -0,0 +1,41 @@
+package day16_notations;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C02_BeforeClass_AfterClass extends TestBase {
+
+    //JUnit te @BeforeClass ve @AfterClass notasyonuna sahip methodlar static olmak zorundaydı
+    //testNG de bu zorunluluktan bizi kurtarıyor
+    /*
+    TestNG bize daha fazla before ve after notasyonları sunar.Diger before ve after notasyonları
+    tanımlaycagımız grup,tets veya suit den once ve sonra calısırlar
+    İleride xml dosyaları ile birlikte bunları gorecegiz
+     */
+
+    @BeforeClass
+    public void beforeClassMethod(){
+        System.out.println("Before Class");
+    }
+    @AfterClass
+    public void afterClassMethod(){
+        System.out.println("After Class");
+    }
+    @Test
+    public void amazonTesti() {
+        driver.get("https://www.amazon.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+    @Test
+    public void test02() {
+        driver.get("https://www.bestbuy.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+    @Test
+    public void techproedTesti() {
+        driver.get("https://www.techproeducation.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+}
---
+++ src/test/java/day16_notations/C03_Priority.java
@@ -0,0 +1,38 @@
+package day16_notations;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C03_Priority extends TestBase {
+    /*
+    TestNG test methodlarını isim sırasına gore calıstırır.Eger isim sıralamasının dısında bir
+     sıralama ile calısmasını ısterseniz o zaman test methodlarını oncelik( priority) tanımlayabiliriz
+     Priority kucukten buyuge gore calısır.
+     Eger bir test methoduna priority degeri atanmamıssa default olarak priority=0 olarak kabul edilir
+     */
+    @BeforeClass
+    public void beforeClassMethod(){
+        System.out.println("Before Class");
+    }
+    @AfterClass
+    public void afterClassMethod(){
+        System.out.println("After Class");
+    }
+    @Test(priority = -5)
+    public void amazonTesti() {
+        driver.get("https://www.amazon.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+    @Test(priority = -2)
+    public void test02() {
+        driver.get("https://www.bestbuy.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+    @Test
+    public void techproedTesti() {
+        driver.get("https://www.techproeducation.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+}
