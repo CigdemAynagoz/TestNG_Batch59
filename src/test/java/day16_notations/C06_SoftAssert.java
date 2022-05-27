004bdc28516075f19b1e3ef71b74d045e460a943
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-05-27 16:14:10 +0300

    27.05.2022

--- src/test/java/day16_notations/C06_SoftAssert.java
+++ src/test/java/day16_notations/C06_SoftAssert.java
@@ -5,32 +5,52 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utilities.TestBase;
 
 public class C06_SoftAssert extends TestBase {
     @Test
     public void test01() {
+        /*
+        Softassertion baslangıc ve bıtıs satirlari arasındaki tum assertion lari yapıp
+        bitis satırına geldiginde bize buldugu tum hataları rapor eder
+         */
+
+        //Softassertion un baslangıcı obje olusturmaktr
+        SoftAssert softAssert=new SoftAssert();
+
         //1-amazon anasayfaya gidin
         driver.get("https://www.amazon.com");
 
         //2-title in Amazon içerdigini test edin
-        String expectedTitle="amazon";
+        String expectedTitle="Amazon";
         String actualTitle=driver.getTitle();
-        Assert.assertTrue(actualTitle.contains(expectedTitle));
+        softAssert.assertTrue(actualTitle.contains(expectedTitle),"title amazon icermiyor");
 
         //3-arama kutusnun erişilebilir oldugunu test edin
         WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
-        Assert.assertTrue(aramaKutusu.isEnabled());
+        softAssert.assertTrue(aramaKutusu.isEnabled(),"arama kutusuna erisilemiyor");
 
         //4-arama kutusuna Nutella yazıp aratın
         aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
 
         //5-arama yapıldıgını test edin
         WebElement sonucYaziElementi= driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
-        Assert.assertTrue(sonucYaziElementi.isDisplayed());
+        softAssert.assertTrue(sonucYaziElementi.isDisplayed(),"arama yapılamadı");
 
         //6-arama sonucunun Nutella içerdigini test edin
-        Assert.assertTrue(sonucYaziElementi.getText().contains("Kutella"));
+        softAssert.assertTrue(sonucYaziElementi.getText().contains("Nutella"),"sonuc yazısı kutella ıcermıyor");
+
+        //softassert e bitis satırını belirtmek icin assertAll() kullanılır
+        softAssert.assertAll();
+        /*
+        Softassert un hata bulsa bile calısmaya devam etme ozelligi asserall a kadardır.
+        eger assertAll da failed rapor edilirse calısma durur ve claassın kalan kısmı calıstırılmaz
+        Yani assertAll HardAssert deki her bir assert gibidir.
+        Hatayi yakalarsa calısma durur.
+         */
+
+        System.out.println("assertion'lardan failed olan olursa burası calısmaz");
 
     }
 }
---
+++ src/test/java/practice/C01_YoutubeAssertion.java
@@ -0,0 +1,56 @@
+package practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C01_YoutubeAssertion extends TestBase {
+    @Test
+    public void test01() {
+        //1) Bir class oluşturun: YoutubeAssertions
+        //2) https://www.youtube.com adresine gidin
+        driver.get("https://www.youtube.com");
+
+        //3) Aşağıdaki adları kullanarak 4 test metodu oluşturun ve gerekli testleri yapin
+        //○ titleTest => Sayfa başlığının “YouTube” oldugunu test edin
+        //○ imageTest => YouTube resminin görüntülendiğini (isDisplayed()) test edin
+        //○ Search Box 'in erisilebilir oldugunu test edin (isEnabled())
+        //○ wrongTitleTest => Sayfa basliginin “youtube” olmadigini dogrulayin
+    }
+
+    @Test
+    public void titleTest() {
+        //○ titleTest => Sayfa başlığının “YouTube” oldugunu test edin
+        driver.get("https://www.youtube.com");
+        String expectedTitle ="YouTube";
+        String actualTitle=driver.getTitle();
+        Assert.assertTrue(expectedTitle.contains(actualTitle),"Sayfa baslıgı Youtube iceriyor");
+    }
+
+    @Test
+    public void imageTest() {
+        //○ imageTest => YouTube resminin görüntülendiğini (isDisplayed()) test edin
+        driver.get("https://www.youtube.com");
+        WebElement youtubeLogo=driver.findElement(By.xpath("(//yt-icon[@id='logo-icon'])[1]"));
+        Assert.assertTrue(youtubeLogo.isDisplayed());
+    }
+
+    @Test
+    public void searchBoxTest() {
+        driver.get("https://www.youtube.com");
+        //○ Search Box 'in erisilebilir oldugunu test edin (isEnabled())
+        WebElement searchBox=driver.findElement(By.xpath("//button[@id='search-icon-legacy']"));
+        Assert.assertTrue(searchBox.isEnabled());
+    }
+
+    @Test
+    public void wrongTitleTest() {
+        //○ wrongTitleTest => Sayfa basliginin “youtube” olmadigini dogrulayin
+        driver.get("https://www.youtube.com");
+        String expectedTitle ="youTube";
+        String actualTitle=driver.getTitle();
+        Assert.assertFalse(actualTitle.contains(expectedTitle));
+    }
+}
