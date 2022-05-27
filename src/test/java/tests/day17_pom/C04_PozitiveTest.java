285f537c56e067fd759835bf0e8a395bece6b266
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-05-27 23:21:07 +0300

    27.05.2022

--- src/test/java/tests/day17_pom/C04_PozitiveTest.java
+++ src/test/java/tests/day17_pom/C04_PozitiveTest.java
@@ -1,2 +1,11 @@
-package tests.day17_pom;public class C04_PozitiveTest {
+package tests.day17_pom;
+
+public class C04_PozitiveTest {
+    //1 ) Bir Class olustur : PositiveTest
+    //2) Bir test method olustur positiveLoginTest()
+    // https://www.hotelmycamp.com/ adresine git
+    // login butonuna bas
+    // test data username: manager ,
+    // test data password : Manager1!
+    // Degerleri girildiginde sayfaya basarili sekilde girilebildigini test et
 }
