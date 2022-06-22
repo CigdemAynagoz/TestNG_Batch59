68a3fd77e25da65ec0845bce6e1e6c07317314b6
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-06-22 22:40:31 +0300

    22.06.2022

--- src/test/java/tests/practice/Tradylinn_Deneme.java
+++ src/test/java/tests/practice/Tradylinn_Deneme.java
@@ -8,14 +8,30 @@ import utilities.Driver;
 public class Tradylinn_Deneme {
     @Test
     public void test01() throws InterruptedException {
+
         TradylinnPage tradylinnPage=new TradylinnPage();
+
         Driver.getDriver().get("https://tradylinn.com/");
+
         tradylinnPage.girisYapButton.click();
+
         Thread.sleep(3000);
+
         tradylinnPage.usernameBox.sendKeys("c.ayngz.03@gmail.com"+ Keys.ENTER);
+
         tradylinnPage.passwordBox.sendKeys("cgdmayngz.1"+ Keys.ENTER);
+
         tradylinnPage.loginButton.click();
+
         Driver.closeDriver();
+
+
+
+
+
+
+
+
 
     }
 }
